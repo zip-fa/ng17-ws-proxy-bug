c642f96e39564dfbc02ff2bb2b595ec543e4a8ff
zip-fa <61551308+zip-fa@users.noreply.github.com>
2023-10-26 21:22:01 +0300

    bug repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WebSocketSubject } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ng17-ws-proxy-bug';
+
+  constructor() {
+    const subject = new WebSocketSubject(`ws://localhost:4200/v3/channel_123`);
+
+    subject.subscribe({
+      next: msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
+      error: err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      complete: () => console.log('complete') // Called when connection is closed (for whatever reason).
+    });
+  }
 }
